e369341fe40175198054ff8457feb674da77cc03
Andrei Neculau <andrei.neculau@gmail.com>
2013-05-31 22:30:09 +0200

    change deps tree

--- bin/tree.sh
+++ bin/tree.sh
@@ -8,7 +8,9 @@ APPENDS=$(head -n25 $FILE | grep " * @append " | cut -d' ' -f4)
 for APPEND in $APPENDS
 do
     if [ -z $(echo "$TREE" | grep "$APPEND") ]; then
-        TREE=$(printf "$TREE\n$PREFIX$APPEND")
+        # TREE=$(printf "$TREE\n$PREFIX$APPEND")
+        # Prepend in order to allow the main file to overwrite rules "apended"
+        TREE=$(printf "$PREFIX$APPEND\n$TREE")
     fi
 done
 echo "$TREE"
